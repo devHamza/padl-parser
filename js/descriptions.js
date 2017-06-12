48b785dad6d94c91cc3d0e3b9a1c08e92268213d
Hamza <hsain.hamza@gmail.com>
2017-06-12 21:13:31 +0000

    Add files via upload

---
+++ js/descriptions.js
@@ -0,0 +1,858 @@
+var descriptions = [
+    {
+        parameter: 'aid',
+        description: 'Application ID'
+    },
+    {
+        parameter: 'an',
+        description: 'application name'
+    },
+    {
+        parameter: 'av',
+        description: 'Application Version'
+    },
+    {
+        parameter: 'cd',
+        description: 'Screen name'
+    },
+    {
+        parameter: 'cd1',
+        description: 'Nestle Zone'
+    },
+    {
+        parameter: 'cd2',
+        description: 'Nestle Country'
+    },
+    {
+        parameter: 'cd3',
+        description: 'Nestle Business'
+    },
+    {
+        parameter: 'cd4',
+        description: 'Nestle Brand'
+    },
+    {
+        parameter: 'cd5',
+        description: 'Nestle Site'
+    },
+    {
+        parameter: 'cd6',
+        description: 'Nestle Properties'
+    },
+    {
+        parameter: 'cd7',
+        description: 'Nestle Type'
+    },
+    {
+        parameter: 'cd8',
+        description: 'Nestle URL'
+    },
+    {
+        parameter: 'cd9',
+        description: 'Nestle Language'
+    },
+    {
+        parameter: 'cd10',
+        description: 'Nestle Technology'
+    },
+    {
+        parameter: 'cd11',
+        description: 'unused'
+    },
+    {
+        parameter: 'cd12',
+        description: 'Nestle DIGIPI'
+    },
+    {
+        parameter: 'cd13',
+        description: 'PROD'
+    },
+    {
+        parameter: 'cd14',
+        description: 'Page Name'
+    },
+    {
+        parameter: 'cd15',
+        description: 'Page Type'
+    },
+    {
+        parameter: 'cd16',
+        description: 'Page Category'
+    },
+    {
+        parameter: 'cd17',
+        description: 'Page Subcategory'
+    },
+    {
+        parameter: 'cd18',
+        description: 'Page Technology'
+    },
+    {
+        parameter: 'cd19',
+        description: 'Market'
+    },
+    {
+        parameter: 'cd20',
+        description: 'Version'
+    },
+    {
+        parameter: 'cd21',
+        description: 'Lanscape'
+    },
+    {
+        parameter: 'cd22',
+        description: 'Segment'
+    },
+    {
+        parameter: 'cd23',
+        description: 'Page ID'
+    },
+    {
+        parameter: 'cd24',
+        description: 'Page Instance ID'
+    },
+    {
+        parameter: 'cd25',
+        description: 'clubMemberTierID'
+    },
+    {
+        parameter: 'cd26',
+        description: 'Currency'
+    },
+    {
+        parameter: 'cd27',
+        description: 'Club Member ID'
+    },
+    {
+        parameter: 'cd28',
+        description: 'User Agent'
+    },
+    {
+        parameter: 'cd29',
+        description: 'Club Member Status'
+    },
+    {
+        parameter: 'cd30',
+        description: 'Club Member Level'
+    },
+    {
+        parameter: 'cd31',
+        description: 'Standing Order_True or False'
+    },
+    {
+        parameter: 'cd32',
+        description: 'Club Member Title'
+    },
+    {
+        parameter: 'cd33',
+        description: 'Club Member Login Status'
+    },
+    {
+        parameter: 'cd34',
+        description: 'Machine Owned'
+    },
+    {
+        parameter: 'cd35',
+        description: 'Preferred Technology'
+    },
+    {
+        parameter: 'cd36',
+        description: 'Shipping Address City'
+    },
+    {
+        parameter: 'cd37',
+        description: 'Shipping Address State'
+    },
+    {
+        parameter: 'cd38',
+        description: 'Shipping Address Country'
+    },
+    {
+        parameter: 'cd39',
+        description: 'Discover Offer - Yes_No'
+    },
+    {
+        parameter: 'cd40',
+        description: 'Billing Address City'
+    },
+    {
+        parameter: 'cd41',
+        description: 'Billing Address State'
+    },
+    {
+        parameter: 'cd42',
+        description: 'Billing Address Country'
+    },
+    {
+        parameter: 'cd43',
+        description: 'Standing Order Product - dimension in GA'
+    },
+    {
+        parameter: 'cd44',
+        description: 'Discover Offer Product - dimension in GA'
+    },
+    {
+        parameter: 'cd45',
+        description: 'Checkout Main Payement Method'
+    },
+    {
+        parameter: 'cd46',
+        description: 'Checkout Shipping Method ID'
+    },
+    {
+        parameter: 'cd47',
+        description: 'Delivery Option - Recycling'
+    },
+    {
+        parameter: 'cd48',
+        description: 'Delivery Option - Priority'
+    },
+    {
+        parameter: 'cd49',
+        description: 'Delivery Option - Signature'
+    },
+    {
+        parameter: 'cd50',
+        description: 'GA Client ID'
+    },
+    {
+        parameter: 'cd51',
+        description: 'User Credit Used'
+    },
+    {
+        parameter: 'cd52',
+        description: 'New Client'
+    },
+    {
+        parameter: 'cd53',
+        description: 'Market Product ID - dimension in GA'
+    },
+    {
+        parameter: 'cd54',
+        description: 'Market Product Name - dimension in GA'
+    },
+    {
+        parameter: 'cd55',
+        description: 'Product Range - dimension in GA'
+    },
+    {
+        parameter: 'cd56',
+        description: 'Product Technology - dimension in GA'
+    },
+    {
+        parameter: 'cd57',
+        description: 'Product Type - dimension in GA'
+    },
+    {
+        parameter: 'cd58',
+        description: 'Machine Owner'
+    },
+    {
+        parameter: 'cd59',
+        description: 'Product Added by User - dimension in GA'
+    },
+    {
+        parameter: 'cd60',
+        description: 'Page Variant'
+    },
+    {
+        parameter: 'cd61',
+        description: 'Campaign Type'
+    },
+    {
+        parameter: 'cd62',
+        description: 'Business Channel'
+    },
+    {
+        parameter: 'cd63',
+        description: 'Campaign Location'
+    },
+    {
+        parameter: 'cd64',
+        description: 'Publisher Name'
+    },
+    {
+        parameter: 'cd65',
+        description: 'Campaign Section'
+    },
+    {
+        parameter: 'cd66',
+        description: 'Category/Type'
+    },
+    {
+        parameter: 'cd67',
+        description: 'Activation Group Target'
+    },
+    {
+        parameter: 'cd68',
+        description: 'Extra Source Attribute'
+    },
+    {
+        parameter: 'cd69',
+        description: 'Creative ID'
+    },
+    {
+        parameter: 'cd70',
+        description: 'Format ID'
+    },
+    {
+        parameter: 'cd71',
+        description: 'Custom Dimension 71 - icid Internal Promo Banner ID'
+    },
+    {
+        parameter: 'cd72',
+        description: 'Custom Dimension 72 - icid Internal Promo Name'
+    },
+    {
+        parameter: 'cd73',
+        description: 'Custom Dimension 73 - icid Internal Promo Placement'
+    },
+    {
+        parameter: 'cd74',
+        description: 'Custom Dimension 74 - icid Internal Promo Format'
+    },
+    {
+        parameter: 'cd75',
+        description: 'Custom Dimension 75 - cid Omniture Campaign ID'
+    },
+    {
+        parameter: 'cd76',
+        description: 'Custom Dimension 76 - did Delivery ID'
+    },
+    {
+        parameter: 'cd77',
+        description: 'Check out step name'
+    },
+    {
+        parameter: 'cd78',
+        description: 'Is Eco Tax - Product-Level Scope'
+    },
+    {
+        parameter: 'cd79',
+        description: 'Product Cup Size - Product-Level Scope'
+    },
+    {
+        parameter: 'cd80',
+        description: 'Product Intensity - Product-Level Scope'
+    },
+    {
+        parameter: 'cd81',
+        description: 'Aromatic Profile - Product-Level Scope'
+    },
+    {
+        parameter: 'cd82',
+        description: 'Source Placement - Session-Level Scope'
+    },
+    {
+        parameter: 'cd83',
+        description: 'Machine Color - Product-Level Scope'
+    },
+    {
+        parameter: 'cd84',
+        description: 'Accessory Collection - Product-Level Scope'
+    },
+    {
+        parameter: 'cd85',
+        description: 'Accessory Finish - Product-Level Scope'
+    },
+    {
+        parameter: 'cd86',
+        description: 'Accessory Type - Product-Level Scope'
+    },
+    {
+        parameter: 'cd87',
+        description: 'Assortment - Product-Level Scope'
+    },
+    {
+        parameter: 'cd88',
+        description: 'Machine Shade - Product-Level Scope'
+    },
+    {
+        parameter: 'cd89',
+        description: 'Machine Manufacturer - Product-Level Scope'
+    },
+    {
+        parameter: 'cd90',
+        description: 'Product Rating - Product-Level Scope'
+    },
+    {
+        parameter: 'cd91',
+        description: 'Product Number of Reviews - Product-Level'
+    },
+    {
+        parameter: 'cd92',
+        description: 'Club Action IDs - Combined - Hit-Level Scope'
+    },
+    {
+        parameter: 'cd93',
+        description: 'Standing Order Frequency - Hit-Level Scope'
+    },
+    {
+        parameter: 'pr1nm',
+        description: 'product name'
+    },
+    {
+        parameter: 'pr1id',
+        description: 'product 1-id'
+    },
+    {
+        parameter: 'pr1pr         ',
+        description: 'product 1-price '
+    },
+    {
+        parameter: 'pr1qt',
+        description: 'product 1-quantity'
+    },
+    {
+        parameter: 'pr1cm5',
+        description: 'machineQuantity         '
+    },
+    {
+        parameter: 'pr1cm6',
+        description: 'capsuleQuantity         '
+    },
+    {
+        parameter: 'pr1cm9',
+        description: 'accessoryQuantity         '
+    },
+    {
+        parameter: 'pr1cm10',
+        description: 'capsuleQuantity '
+    },
+    {
+        parameter: 'pr1cm11',
+        description: 'machineQuantity'
+    },
+    {
+        parameter: 'pr1cm12',
+        description: 'accessoryQuantity    '
+    },
+    {
+        parameter: 'pr1cm13',
+        description: 'capsuleQuantity '
+    },
+    {
+        parameter: 'pr1cm14',
+        description: 'machineQuantity'
+    },
+    {
+        parameter: 'pr1cm15',
+        description: 'accessoryQuantity    '
+    },
+    {
+        parameter: 'pr1cm16',
+        description: 'capsuleQuantity '
+    },
+    {
+        parameter: 'pr1cm17',
+        description: 'machineQuantity'
+    },
+    {
+        parameter: 'pr1cm18',
+        description: 'accessoryQuantity    '
+    },
+    {
+        parameter: 'pr1br',
+        description: 'product 1-brand'
+    },
+    {
+        parameter: 'pr1ca',
+        description: 'product 1-category'
+    },
+    {
+        parameter: 'pr1cd53',
+        description: 'product 1-dimension53 - productID'
+    },
+    {
+        parameter: 'pr1cd54',
+        description: 'product 1-dimension54 - productName'
+    },
+    {
+        parameter: 'pr1cd55',
+        description: 'product 1-dimension55 - secondaryCategory'
+    },
+    {
+        parameter: 'pr1cd56',
+        description: 'product 1-dimension56 - product Technology'
+    },
+    {
+        parameter: 'pr1cd57',
+        description: 'product 1-dimension57 - product type'
+    },
+    {
+        parameter: 'pr1cd59',
+        description: 'product 1-dimension59 - is added by user'
+    },
+    {
+        parameter: 'v',
+        description: 'protocol version'
+    },
+    {
+        parameter: 'aip',
+        description: 'Anonymize IP'
+    },
+    {
+        parameter: 'ds',
+        description: 'Data Source'
+    },
+    {
+        parameter: 'qt',
+        description: 'Queue Time'
+    },
+    {
+        parameter: 'z',
+        description: 'Cache Buster'
+    },
+    {
+        parameter: 'cid',
+        description: 'Client ID'
+    },
+    {
+        parameter: 'tid',
+        description: 'property ID'
+    },
+    {
+        parameter: 't',
+        description: 'Event hit type'
+    },
+    {
+        parameter: 'ec',
+        description: 'Event Category'
+    },
+    {
+        parameter: 'ea',
+        description: 'Event Action'
+    },
+    {
+        parameter: 'el',
+        description: 'Event label'
+    },
+    {
+        parameter: 'ev',
+        description: 'Event value'
+    },
+    {
+        parameter: 'uid',
+        description: 'user ID'
+    },
+    {
+        parameter: 'sc',
+        description: 'Session Control'
+    },
+    {
+        parameter: 'uip',
+        description: 'IP Override'
+    },
+    {
+        parameter: 'ua',
+        description: 'User Agent'
+    },
+    {
+        parameter: 'geoid',
+        description: 'The geographical location of the user'
+    },
+    {
+        parameter: 'dr',
+        description: 'Document Referrer'
+    },
+    {
+        parameter: 'cn',
+        description: 'Campaign Name'
+    },
+    {
+        parameter: 'cm',
+        description: 'Campaign Medium'
+    },
+    {
+        parameter: 'ck',
+        description: 'campaign keyword.'
+    },
+    {
+        parameter: 'cc',
+        description: 'campaign content'
+    },
+    {
+        parameter: 'cid',
+        description: 'campaign ID.'
+    },
+    {
+        parameter: 'gclid',
+        description: 'Google AdWords Id'
+    },
+    {
+        parameter: 'dclid',
+        description: 'Google Display Ads Id'
+    },
+    {
+        parameter: 'sr',
+        description: 'screen resolution'
+    },
+    {
+        parameter: 'vp',
+        description: 'viewable area of the browser / device.'
+    },
+    {
+        parameter: 'de',
+        description: 'Document Encoding'
+    },
+    {
+        parameter: 'sd',
+        description: 'screen color'
+    },
+    {
+        parameter: 'ul',
+        description: 'User language'
+    },
+    {
+        parameter: 'je',
+        description: 'Java enabled'
+    },
+    {
+        parameter: 'fl',
+        description: 'flash version'
+    },
+    {
+        parameter: 'ni',
+        description: 'Non-Interaction Hit'
+    },
+    {
+        parameter: 'dl',
+        description: 'document location URL'
+    },
+    {
+        parameter: 'dh',
+        description: 'document hostname'
+    },
+    {
+        parameter: 'dp',
+        description: 'document path'
+    },
+    {
+        parameter: 'dt',
+        description: 'document title'
+    },
+    {
+        parameter: 'cg1',
+        description: 'Content Group 1'
+    },
+    {
+        parameter: 'cg2',
+        description: 'Content Group 2'
+    },
+    {
+        parameter: 'cg3',
+        description: 'Content Group 3'
+    },
+    {
+        parameter: 'cg4',
+        description: 'Content Group 4'
+    },
+    {
+        parameter: 'cg5',
+        description: 'Content Group 5'
+    },
+    {
+        parameter: 'linkid',
+        description: 'Link ID'
+    },
+    {
+        parameter: 'aiid',
+        description: 'Application installer identifier'
+    },
+    {
+        parameter: 'ti',
+        description: 'transaction id'
+    },
+    {
+        parameter: 'ta',
+        description: 'transaction affiliation'
+    },
+    {
+        parameter: 'tr',
+        description: 'transaction revenue'
+    },
+    {
+        parameter: 'ts',
+        description: 'transaction shipping cost'
+    },
+    {
+        parameter: 'tt',
+        description: 'transaction tax total'
+    },
+    {
+        parameter: 'in',
+        description: 'item name'
+    },
+    {
+        parameter: 'ip',
+        description: 'item price'
+    },
+    {
+        parameter: 'iq',
+        description: 'item quantity'
+    },
+    {
+        parameter: 'ic',
+        description: 'item code'
+    },
+    {
+        parameter: 'iv',
+        description: 'item category'
+    },
+    {
+        parameter: 'pa',
+        description: 'product action'
+    },
+    {
+        parameter: 'pal',
+        description: 'product action list'
+    },
+    {
+        parameter: 'cos',
+        description: 'checkout step'
+    },
+    {
+        parameter: 'col',
+        description: 'checkout step option'
+    },
+    {
+        parameter: 'il1nm',
+        description: 'product impression list name'
+    },
+    {
+        parameter: 'il1pi1id',
+        description: 'product impression SKU'
+    },
+    {
+        parameter: 'il1pi1nm',
+        description: 'product impression name'
+    },
+    {
+        parameter: 'il1pi1br',
+        description: 'product impression brand'
+    },
+    {
+        parameter: 'il1pi1ca',
+        description: 'product impression category'
+    },
+    {
+        parameter: 'il1pi1va',
+        description: 'product impression variant'
+    },
+    {
+        parameter: 'il1pi1ps',
+        description: 'product impression position'
+    },
+    {
+        parameter: 'il1pi1pr',
+        description: 'product impression price'
+    },
+    {
+        parameter: 'il1pi1cd1',
+        description: 'Product Impression Custom Dimension'
+    },
+    {
+        parameter: 'il1pi1cm1',
+        description: 'Product Impression Custom Metric'
+    },
+    {
+        parameter: 'promo1id',
+        description: 'Promotion ID'
+    },
+    {
+        parameter: 'promo1nm',
+        description: 'Promotion Name'
+    },
+    {
+        parameter: 'promo1cr',
+        description: 'Promotion Creative'
+    },
+    {
+        parameter: 'promo1ps',
+        description: 'Promotion Position'
+    },
+    {
+        parameter: 'promoa',
+        description: 'Promotion Action'
+    },
+    {
+        parameter: 'cu',
+        description: 'Currency Code'
+    },
+    {
+        parameter: 'sn',
+        description: 'Social Network'
+    },
+    {
+        parameter: 'sa',
+        description: 'Social Action'
+    },
+    {
+        parameter: 'st',
+        description: 'Social Action Target'
+    },
+    {
+        parameter: 'utc',
+        description: 'User timing category'
+    },
+    {
+        parameter: 'utv',
+        description: 'User timing variable name'
+    },
+    {
+        parameter: 'utt',
+        description: 'User timing time'
+    },
+    {
+        parameter: 'utl',
+        description: 'User timing label'
+    },
+    {
+        parameter: 'plt',
+        description: 'Page Load Time'
+    },
+    {
+        parameter: 'dns',
+        description: 'DNS Time'
+    },
+    {
+        parameter: 'pdt',
+        description: 'Page Download Time'
+    },
+    {
+        parameter: 'rtt',
+        description: 'Redirect Response Time'
+    },
+    {
+        parameter: 'tcp',
+        description: 'TCP Connect Time'
+    },
+    {
+        parameter: 'srt',
+        description: 'Server Response Time'
+    },
+    {
+        parameter: 'dit',
+        description: 'DOM Interactive Time'
+    },
+    {
+        parameter: 'clt',
+        description: 'Content Load Time'
+    },
+    {
+        parameter: 'exd',
+        description: 'Exception Description'
+    },
+    {
+        parameter: 'exf',
+        description: 'Is Exception Fatal?'
+    },
+    {
+        parameter: 'xid',
+        description: 'Experiment ID'
+    },
+    {
+        parameter: 'xvar',
+        description: 'Experiment Variant'
+    }
+];
